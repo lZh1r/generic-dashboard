d90b18f64234d5e1d8f8f2181431eaadb2c51d8a
lZh1r <electroxhackerpig09@gmail.com>
2025-05-19 09:15:03 +0300

    bar chart

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,11 @@
+import AppBarChart from "@/components/AppBarChart";
+
 export default function Home() {
     return (
-        <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+            <div className="rounded-2xl bg-primary-foreground p-4 col-span-2">
+                <AppBarChart/>
+            </div>
 
         </div>
     );
