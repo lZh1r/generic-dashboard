2d4a450a6336b77eea508271fafa5ff0f16e4216
lZh1r <electroxhackerpig09@gmail.com>
2025-05-19 09:35:18 +0300

    area chart

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,26 @@
 import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
 
 export default function Home() {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
             <div className="rounded-2xl bg-primary-foreground p-4 col-span-2">
                 <AppBarChart/>
+            </div>
+            <div className="rounded-2xl bg-primary-foreground p-4">
+                <p>Test</p>
+            </div>
+            <div className="rounded-2xl bg-primary-foreground p-4">
+                <p>Test</p>
+            </div>
+            <div className="rounded-2xl bg-primary-foreground p-4">
+                <p>Test</p>
+            </div>
+            <div className="rounded-2xl bg-primary-foreground p-4 col-span-2">
+                <AppAreaChart/>
+            </div>
+            <div className="rounded-2xl bg-primary-foreground p-4">
+                <p>Test</p>
             </div>
 
         </div>
