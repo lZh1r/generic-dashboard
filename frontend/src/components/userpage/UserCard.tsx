3ca722d93c3378a2104a9a92132f882a3dd8330e
lZh1r <electroxhackerpig09@gmail.com>
2025-05-24 11:31:21 +0300

    User avatar and display name in UserCard.tsx

--- frontend/src/components/userpage/UserCard.tsx
+++ frontend/src/components/userpage/UserCard.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {zustandStore} from "@/lib/zustand-store";
+import {useParams} from "next/navigation";
+import {capitalize} from "@/lib/lib";
 
 function UserCard() {
+
+    const params = useParams<{username: string}>();
+    const isLoggedUser = params.username === zustandStore((state) => state.username);
+    const displayName = zustandStore((state) => state.displayName);
+
     return (
-        <div></div>
+        <div className="rounded-lg bg-primary-foreground p-4">
+            <div className="flex gap-4 items-center">
+                <Avatar>
+                    <AvatarImage src="https://avatars.githubusercontent.com/u/126076657?v=4"/>
+                    <AvatarFallback>PF</AvatarFallback>
+                </Avatar>
+                <h1 className="text-lg font-medium">{
+                    isLoggedUser ? displayName : capitalize(params.username)
+                }</h1>
+            </div>
+
+        </div>
     );
 }
 
